64b9ed14b3f86f17aa653873e08bd673fb29fde9
Matthew Wright <matthew@smallhelm.com>
2015-05-13 10:29:33 -0600

    updating less to 2.5.0 and using clean-css to minify

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 var fs = require('fs');
 var less = require('less');
+var CleanCSS = require('clean-css');
 
 var css_to_js = function(css){
-	//see css-to-js-fn.template.js
-	return '(function(e){if("undefined"!=typeof document){var t=document;if(t.less_to_js__inserted=t.less_to_js__inserted||{},!t.less_to_js__inserted[e]){t.less_to_js__inserted[e]=!0;var s=t.createElement("style");s.setAttribute("type","text/css"),"textContent"in s?s.textContent=e:s.styleSheet.cssText=e,t.getElementsByTagName("head")[0].appendChild(s)}}}(' + JSON.stringify(css) + "));";
+  //see css-to-js-fn.template.js
+  return '(function(e){if("undefined"!=typeof document){var t=document;if(t.less_to_js__inserted=t.less_to_js__inserted||{},!t.less_to_js__inserted[e]){t.less_to_js__inserted[e]=!0;var s=t.createElement("style");s.setAttribute("type","text/css"),"textContent"in s?s.textContent=e:s.styleSheet.cssText=e,t.getElementsByTagName("head")[0].appendChild(s)}}}(' + JSON.stringify(css) + "));";
+};
+
+var minifyCSS = function(css){
+  return new CleanCSS({
+    advanced: true,
+    keepSpecialComments: 0
+  }).minify(css).styles;
 };
 
 module.exports = function(file, callback){
-	fs.readFile(file, {encoding: "utf8"}, function(err, data){
-		if(err)
-			return callback(err);
+  fs.readFile(file, {encoding: "utf8"}, function(err, data){
+    if(err) return callback(err);
 
-		less.render(data.toString(), {compress: true}, function(err, out){
-			if(err)
-				return callback(err);
+    less.render(data.toString(), {}, function(err, out){
+      if(err) return callback(err);
 
-			fs.writeFile(file + ".js", css_to_js(out.css), {encoding: "utf8"}, function(err){
-				 callback(err || null);
-			});
-		});
-	});
+      fs.writeFile(file + ".js", css_to_js(minifyCSS(out.css)), {encoding: "utf8"}, callback);
+    });
+  });
 };
